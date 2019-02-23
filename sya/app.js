011aaf7358f704b27d5823100236167ef72b6a23
Olcay Bayram <olcaybayram@gmail.com>
2019-02-23 20:00:27 +0100

    Music player script

---
+++ sya/app.js
@@ -0,0 +1,7 @@
+var audioControl = '<audio controls autoplay>'
++'<source src="http://otomatikmuhendis.com/sya/bizolmasak.m4a" type="audio/mp4" />'
++'</audio>';
+
+
+var scrAudio = document.getElementById('scrAudio');
+scrAudio.insertAdjacentHTML("afterend", audioControl);
